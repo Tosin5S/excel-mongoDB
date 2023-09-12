9ae306a166bbd9939022d3b25268888c34bb59e1
unknown <olyguy.aq@gmail.com>
2023-09-12 12:29:03 +0100

    update MongoDB using an ODM like mongoose

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const multer = require('multer');
+const xlsx = require('xlsx');
+const mongoose = require('mongoose');
 const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 
-app.post('/upload', upload.single('file'), (req, res) => {
+// Define a schema for transactions 
+const transactionsSchema = new mongoose.Schema({
+    // Define your transaction fields here
+})
+
+const Transaction = mongoose.model('Transaction', transactionsSchema);
+ 
+
+
+app.post('/upload', upload.single('file'), async (req, res) => {
   // Handle the uploaded file here
   const file = req.file;
+  const workbook = xlsx.readFile(file.path);
+  const sheetName = workbook.SheetNames[0];
+  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
   // Process the Excel file and update MongoDB
-  // ...
-  res.send('File uploaded successfully.');
+  try {
+    // Insert the data into MongoDB
+    await Transaction.insertMany(data);
+    res.send('File uploaded and data inserted into MongoDB successfully.');
+  } catch (error) {
+        console.error('Error inserting data into MongoDB:', error);
+        res.status(500).send('Internal server error');
+    }
 });
 
 app.listen(3000, () => {

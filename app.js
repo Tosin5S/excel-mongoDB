d25edc9279c9206db38f7456835b2f8a94774a32
unknown <olyguy.aq@gmail.com>
2023-09-29 21:57:49 +0100

    mongo-xlsx

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const multer = require('multer');
-const xlsx = require('xlsx');
+const mongoXlsx = require('mongo-xlsx');
 const mongoose = require('mongoose');
 const upload = multer({ dest: 'uploads/' });
+const cors = require("cors");
 
 const app = express();
 
+var corsOptions = {
+  origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// DB Config
+const dbConfig =require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+db.mongoose
+   .connect(
+      dbConfig,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+   )
+   .then(() => {
+      console.log("MongoDB successfully connected")
+      initial();
+   })
+   .catch(err => {
+      console.error("connection error", err);
+      process.exit();
+   });
+   
 // Define a schema for transactions 
 const transactionsSchema = new mongoose.Schema({
     // Define your transaction fields here
@@ -17,22 +45,38 @@ const Transaction = mongoose.model('Transaction', transactionsSchema);
 
 app.post('/upload', upload.single('file'), async (req, res) => {
   // Handle the uploaded file here
-  const file = req.file;
-  const workbook = xlsx.readFile(file.path);
-  const sheetName = workbook.SheetNames[0];
-  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+//  const file = req.file;
+//  const workbook = xlsx.readFile(file.path);
+//  const sheetName = workbook.SheetNames[0];
+//  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
   // Process the Excel file and update MongoDB
-  try {
+//  try {
     // Insert the data into MongoDB
-    await Transaction.insertMany(data);
-    res.send('File uploaded and data inserted into MongoDB successfully.');
-  } catch (error) {
-        console.error('Error inserting data into MongoDB:', error);
-        res.status(500).send('Internal server error');
-    }
-});
+//    await Transaction.insertMany(data);
+//    res.send('File uploaded and data inserted into MongoDB successfully.');
+//  } catch (error) {
+//        console.error('Error inserting data into MongoDB:', error);
+//        res.status(500).send('Internal server error');
+//    }
+// });
 
+// Use mongo-xlsx to parse the Excel file
+const model = mongoXlsx.xlsxToModel(file.path);
+
+// Extract the data frome the model
+const data = model[0].data;
+
+// Insert the data into MongoDB
+Transaction.insertMany(data, (err) => {
+  if (err) {
+    console.error('Error inserting data into MongoDB:', err);
+    res.status(500).send('Internal server error');
+  } else {
+    res.send('File uploaded and data inserted into MongoDB successfully');
+  }
+});
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
